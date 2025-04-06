8345058b5ad9b51e07f777cefac553460d2335e5
gdemartino93 <g.demartino9393@gmail.com>
2025-04-07 00:41:17 +0200

    Revert "test install pwa on ios"
    This reverts commit a18a780a291ef2714aee86edc6d7d33d823d0b96.

--- barber-shop/src/app/app.component.ts
+++ barber-shop/src/app/app.component.ts
@@ -10,8 +10,38 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'barber-shop';
-  
-  constructor() {
+  deferredPrompt: any;
+  showInstallButton = false;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      if (this.isIos() && !this.isInStandaloneMode()) {
+        this.showInstallButton = true; // O mostra messaggio personalizzato
+      } else {
+        window.addEventListener('beforeinstallprompt', (e) => {
+          e.preventDefault();
+          this.deferredPrompt = e;
+          this.showInstallButton = true;
+        });
+      }
+    }
+
   }
 
+  public isIos(): boolean {
+    return /iphone|ipad|ipod/.test(window.navigator.userAgent.toLowerCase());
+  }
+
+  public isInStandaloneMode(): boolean {
+    return ('standalone' in window.navigator) && Boolean((window.navigator as any).standalone);
+  }
+
+  public installPWA() {
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+      this.deferredPrompt.userChoice.then(() => {
+        this.deferredPrompt = null;
+        this.showInstallButton = false;
+      });
+    }
+  }
 }
