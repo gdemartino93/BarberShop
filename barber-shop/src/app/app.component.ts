b38584860cd5f61b483d57961998800d4eb665bc
gdemartino93 <g.demartino9393@gmail.com>
2025-04-07 00:50:48 +0200

    restore routes

--- barber-shop/src/app/app.component.ts
+++ barber-shop/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
