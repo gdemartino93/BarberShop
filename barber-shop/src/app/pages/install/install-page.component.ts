efc53daeef3c5963ac91c4116d7042bd645dc1ec
gdemartino93 <g.demartino9393@gmail.com>
2025-04-07 00:32:56 +0200

    test install pwa

--- barber-shop/src/app/pages/install/install-page.component.ts
+++ barber-shop/src/app/pages/install/install-page.component.ts
@@ -1,5 +1,6 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-install-page',
@@ -9,28 +10,46 @@ import { Component } from '@angular/core';
 })
 export class InstallPageComponent {
   deferredPrompt: any;
+  showInstallButton = false;
+  showIosOverlay = false;
+  private router = inject(Router);
 
   constructor() {
+    if (this.isInStandaloneMode()) {
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    if (this.isIos() && !this.isInStandaloneMode()) {
+      this.showIosOverlay = true;
+    }
+
     window.addEventListener('beforeinstallprompt', (e) => {
       e.preventDefault();
       this.deferredPrompt = e;
+      this.showInstallButton = true;
     });
   }
 
-  installPWA() {
+  public closeIosOverlay() {
+    this.showIosOverlay = false;
+  }
+
+  public installPWA() {
     if (this.deferredPrompt) {
       this.deferredPrompt.prompt();
       this.deferredPrompt.userChoice.then(() => {
         this.deferredPrompt = null;
+        this.showInstallButton = false;
       });
     }
   }
 
-  isIos(): boolean {
+  public isIos(): boolean {
     return /iphone|ipad|ipod/.test(window.navigator.userAgent.toLowerCase());
   }
 
-  isInStandaloneMode(): boolean {
+  public isInStandaloneMode(): boolean {
     return ('standalone' in window.navigator) && Boolean((window.navigator as any).standalone);
   }
 }
