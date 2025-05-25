421d61c3eeca3c764964f5dd86c46c7dbde51920
gdemartino93 <g.demartino9393@gmail.com>
2025-05-26 00:15:31 +0200

    test build

---
+++ barber-shop/dist/barber-shop/browser/chunk-G2INYL7L.js
@@ -0,0 +1,1 @@
+import{Ga as n,Ha as m,Ma as p,wa as o}from"./chunk-IKG6YUVS.js";var a=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-home"]],decls:2,vars:0,template:function(e,i){e&1&&(n(0,"p"),p(1,"home works!"),m())},encapsulation:2})};export{a as HomeComponent};
